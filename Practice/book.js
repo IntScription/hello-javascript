9fef9d75e68483dd66ca6ec83039b9ed356b2a52
Kartik Sanil <22kartiksanil@gmail.com>
2024-07-23 00:51:57 +0530

    update

--- Practice/book.js
+++ Practice/book.js
@@ -5,22 +5,22 @@ function book(name, author, pages, read) {
   this.read = read;
   this.info = function () {
     console.log(
-      this.name + ", " + this.author + ", " + this.pages + ", " + this.read,
+      this.name + ', ' + this.author + ', ' + this.pages + ', ' + this.read
     );
   };
 }
 
 const book1 = new book(
-  "Percy Jackson",
-  "Rick Riordan",
-  "295 pages",
-  "not read yet",
+  'Percy Jackson',
+  'Rick Riordan',
+  '295 pages',
+  'not read yet'
 );
 const book2 = new book(
-  "48 Laws of Power",
-  "Robert Greene",
-  "300 pages or so",
-  "not read yet",
+  '48 Laws of Power',
+  'Robert Greene',
+  '300 pages or so',
+  'not read yet'
 );
 book1.info(); // logs the first book and it's author
 book2.info(); // logs the second book and it's author
