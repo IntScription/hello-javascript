389ce85ec34854b6b1b4053c1b6b2f0c5b4fb840
Kartik Sanil <22kartiksanil@gmail.com>
2024-08-06 14:02:25 +0530

    typeof operator

---
+++ Practice/typeof.js
@@ -0,0 +1,9 @@
+// typeof is a unary operator that returns a string indicating the type of the operand
+console.log(typeof 42);
+console.log(typeof 'hello');
+console.log(typeof true);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () {});
+console.log(typeof undefined);
+console.log(typeof null);
