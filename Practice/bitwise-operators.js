2e1e59902564eaa8332f81e7d83fff6c84bc6a65
Kartik Sanil <22kartiksanil@gmail.com>
2025-10-24 13:16:36 +0530

    bitwise operators

---
+++ Practice/bitwise-operators.js
@@ -0,0 +1,8 @@
+let a = 5; // binary: 101
+let b = 2; // binary: 010
+console.log(a & b); // binary 000 which is 0
+console.log(a | b); // binary 111 which is 7
+console.log(a ^ b); // xor operation binary 111 which is 7
+console.log(~a); // outputs -6
+console.log(a << 1); // outputs 10
+console.log(a >> 1); // outputs 2
