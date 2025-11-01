da80eca00f97ac89a9161f70a3ca410c45f10a8d
Kartik Sanil <22kartiksanil@gmail.com>
2025-11-01 13:22:00 +0530

    update

---
+++ Practice/Strings/strings.js
@@ -0,0 +1,10 @@
+let greeting = 'hello';
+console.log(greeting[1]); // Outputs e
+console.log(greeting[greeting.length - 1]); // Outputs o
+
+let second = greeting[0] + greeting[1];
+console.log(second); // Outputs he
+
+let poem =
+  'Rose are red, \n Violets are blue, \n JavaScript is fun, \n And so are you;';
+console.log(poem);
