a32032b7d26188c0127aa2694182a8c1a0516cf3
Kartik Sanil <22kartiksanil@gmail.com>
2025-11-01 13:22:12 +0530

    updates

--- Practice/repeat-string.js
+++
@@ -1,9 +0,0 @@
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
-  }
-  return string;
-};
-console.log(repeatString("hey", 3)); // returns heyheyhey
--- Practice/reverse-string.js
+++
@@ -1,4 +0,0 @@
-function reverseString(string) {
-  return string.split("").reverse().join("");
-}
-console.log(reverseString("Hello World"));
