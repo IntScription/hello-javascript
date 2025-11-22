e3c8e2c001ca106924e930c93ad398153ffd18d4
Kartik Sanil <22kartiksanil@gmail.com>
2025-11-22 18:59:38 +0530

    updates

--- Practice/Strings/string-truncate.js
+++ Practice/Strings/string-truncate.js
@@ -2,4 +2,4 @@ const strTrunc = (str, num) => {
   return str.length > num ? str.slice(0, num) + '...' : str;
 };
 
-console.log(strTrunc('Hello World', 5));
+console.log(strTrunc('Hello World', 5)); // outputs: Hello...
