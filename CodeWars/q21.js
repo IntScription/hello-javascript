12bbd1cf5f6eb8b1b10bf05b5dde89687551ee02
Kartik Sanil <22kartiksanil@gmail.com>
2025-11-25 18:58:47 +0530

    question 21

---
+++ CodeWars/q21.js
@@ -0,0 +1,16 @@
+/*
+Very simple, given a number, find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+const oppo = (n) => {
+  return -1 * n;
+};
+
+console.log(oppo(2));
+console.log(oppo(-7));
