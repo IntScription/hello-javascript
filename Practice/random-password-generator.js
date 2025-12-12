c1ab807b474158c369551a604a2115345e4c649a
Kartik Sanil <22kartiksanil@gmail.com>
2025-12-12 16:51:14 +0530

    random + crypto

--- Practice/random-password-generator.js
+++ Practice/random-password-generator.js
@@ -1,6 +1,8 @@
+const chars =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+
+// using random method
 const generator = (l) => {
-  const chars =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
   let password = '';
 
   for (let i = 0; i < l; i++) {
@@ -13,3 +15,12 @@ const generator = (l) => {
 
 let password = generator(4);
 console.log(password);
+
+// most secured way, used in node + browsers
+const gen = (len) => {
+  const arr = new Uint32Array(len);
+  crypto.getRandomValues(arr);
+  return Array.from(arr, (x) => chars[x % chars.length]).join('');
+};
+
+console.log(gen(6));
